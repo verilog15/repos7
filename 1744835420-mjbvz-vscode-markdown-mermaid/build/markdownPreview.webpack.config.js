e270f85de03806f444f4711fb1c1626f708d0ffd
verilog15 <verilog15@users.noreply.github.com>
2025-04-16 23:31:24 +0300

    Add mjbvz/vscode-markdown-mermaid at 1744835420

---
+++ 1744835420-mjbvz-vscode-markdown-mermaid/build/markdownPreview.webpack.config.js
@@ -0,0 +1,21 @@
+// @ts-check
+const path = require('path');
+const shared = require('./shared.mermaid.webpack.config');
+const webpack = require('webpack');
+
+module.exports = {
+    ...shared,
+    target: 'web',
+    entry: {
+        'index': path.join(__dirname, '..', 'src', 'markdownPreview', 'index.ts'),
+    },
+    output: {
+        path: path.join(__dirname, '..', 'dist-preview'),
+        filename: '[name].bundle.js'
+    },
+    plugins: [
+        new webpack.optimize.LimitChunkCountPlugin({
+            maxChunks: 1,
+        }),
+    ],
+};
---
+++ 1744835420-mjbvz-vscode-markdown-mermaid/src/notebook/index.ts
@@ -0,0 +1,50 @@
+/**
+ * Main entrypoint for rendering notebooks.
+ * 
+ * Unlike the markdown preview where the Mermaid syntax support and rendering are split between the extension and the webview,
+ * for notebooks everything happens inside of the notebook's webview.
+ * 
+ */
+import type * as MarkdownIt from 'markdown-it';
+import mermaid from 'mermaid';
+import type { RendererContext } from 'vscode-notebook-renderer';
+import { extendMarkdownItWithMermaid } from '../shared-md-mermaid';
+import { loadMermaidConfig, registerMermaidAddons, renderMermaidBlocksInElement } from '../shared-mermaid';
+
+interface MarkdownItRenderer {
+    extendMarkdownIt(fn: (md: MarkdownIt) => void): void;
+}
+
+export async function activate(ctx: RendererContext<void>) {
+    const markdownItRenderer = await ctx.getRenderer('vscode.markdown-it-renderer') as MarkdownItRenderer | undefined;
+    if (!markdownItRenderer) {
+        throw new Error(`Could not load 'vscode.markdown-it-renderer'`);
+    }
+
+    mermaid.initialize(loadMermaidConfig());
+    await registerMermaidAddons();
+
+    markdownItRenderer.extendMarkdownIt((md: MarkdownIt) => {
+        extendMarkdownItWithMermaid(md, { languageIds: () => ['mermaid'] });
+
+        const render = md.renderer.render;
+        md.renderer.render = function (tokens, options, env) {
+            const result = render.call(this, tokens, options, env);
+            const shadowRoot = document.getElementById(env?.outputItem.id)?.shadowRoot;
+
+            const temp = document.createElement('div');
+            temp.innerHTML = result;
+            renderMermaidBlocksInElement(temp, (mermaidContainer, content) => {
+                // The original element we are rendering to has been disconnected.
+                const liveEl = shadowRoot?.getElementById(mermaidContainer.id);
+                if (liveEl) {
+                    liveEl.innerHTML = content;
+                } else {
+                    console.warn('Could not find live element to render mermaid to');
+                }
+            });
+            return temp.innerHTML;
+        };
+        return md;
+    });
+};
